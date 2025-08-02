06b85e0e9a5a597470b4eb99df097901e5f08733
カピ <watashino.pc@gmail.com>
2025-08-02 13:24:07 +0900

    Update app.py
    日本語を消した

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render が使うポートを指定
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
